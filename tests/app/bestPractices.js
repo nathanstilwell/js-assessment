585e7e8cae7b8ec1d92f9a9cdc5dff479a2f0c78
Nathan Stilwell <nathanstilwell@gmail.com>
2015-10-20 23:43:56 -0400

    best practice test passing

--- tests/app/bestPractices.js
+++ tests/app/bestPractices.js
@@ -1,5 +1,8 @@
+/* I added globals to get my linter to stop complaining. :) */
+/*global describe, it */
+
 if ( typeof window === 'undefined' ) {
-  require('../../app/bestPractices');
+  var bestPracticesAnswers = require('../../app/bestPractices');
   var expect = require('chai').expect;
 }
 
