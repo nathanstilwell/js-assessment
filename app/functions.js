e8d91992d34beb95330374c933e49665cb74608a
Nathan Stilwell <nathanstilwell@gmail.com>
2016-01-27 09:12:30 -0500

    pass currying test (less magical than I thought)

--- app/functions.js
+++ app/functions.js
@@ -54,6 +54,12 @@ exports.functionsAnswers = {
   },
 
   curryIt : function(fn) {
-    // skip
+    return function (a) {
+      return function (b) {
+        return function (c) {
+          return fn(a, b, c);
+        };
+      };
+    };
   }
 };
